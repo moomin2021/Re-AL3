df5cf0bb42c1290e5089c6757b462c89936f8e88
tamura_fumiya <k021g1134@g.neec.ac.jp>
2022-05-09 14:12:21 +0900

    re_02_01_ベクトルと行列_完了

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,10 +1,15 @@
 ﻿#include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
+#include "AxisIndicator.h"
+#include "PrimitiveDrawer.h"
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() {
+	delete model_;
+	delete debugCamera_;
+}
 
 void GameScene::Initialize() {
 
@@ -12,9 +17,37 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
+
+	// ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("mario.jpg");
+
+	// 3Dモデルの生成
+	model_ = Model::Create();
+
+	// ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+
+	// ビュープロジェクション
+	viewProjection_.Initialize();
+
+	// デバックカメラの生成
+	debugCamera_ = new DebugCamera(WinApp::kWindowWidth, WinApp::kWindowHeight);
+
+	// 軸方向表示の表示を有効にする
+	AxisIndicator::GetInstance()->SetVisible(true);
+
+	// 軸方向表示が参照するビュープロジェクションを指定する（アドレス無し）
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
+
+	// ライン描画が参照するビュープロジェクションを指定する（アドレス無し）
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&debugCamera_->GetViewProjection());
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+
+	// デバックカメラの更新
+	debugCamera_->Update();
+}
 
 void GameScene::Draw() {
 
@@ -42,6 +75,14 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	
+	// 3Dモデル追加
+	//model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+
+	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
+	
+	// ライン描画が参照するビュープロジェクションを指定する（アドレス無し）
+	PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(0.0f, 0.0f, 5.0f), Vector3(0.0f, 0.0f, -5.0f), Vector4(0.0f, 0.0f, 1.0f, 1.0f));
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -9,6 +9,7 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
+#include "DebugCamera.h"
 
 /// <summary>
 /// ゲームシーン
@@ -50,4 +51,19 @@ class GameScene {
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
+	
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+
+	// 3Dモデル
+	Model * model_ = nullptr;
+
+	// ワールドトランスフォーム
+	WorldTransform worldTransform_;
+
+	// ビュープロジェクション
+	ViewProjection viewProjection_;
+
+	// デバッグのカメラ
+	DebugCamera * debugCamera_ = nullptr;
 };
